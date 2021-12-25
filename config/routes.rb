fad209290075da60968911969700d8daaa71f204
notte779 <mlp15amity@gmail.com>
2021-12-25 12:23:59 +0000

    Add homes-controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  scope module: :public do
+    root to: 'homes#top'
+    get '/about' => 'homes#about', as: 'about'
+  end
 end
