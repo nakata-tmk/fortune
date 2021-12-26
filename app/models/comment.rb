6d16e8fac61483584ae74baaf76ee38d23dd7311
notte779 <mlp15amity@gmail.com>
2021-12-26 12:30:25 +0000

    Update models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,5 @@
 class Comment < ApplicationRecord
+  belongs_to :word
+
+  validates :body, presence: true, length: { maximum: 30 }
 end
--- app/models/word.rb
+++ app/models/word.rb
@@ -1,2 +1,6 @@
 class Word < ApplicationRecord
+  has_many :comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+
+  validates :body, presence: true, length: { maximum: 50 }
 end
