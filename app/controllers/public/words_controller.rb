b2dfdf33e3fd6e08a476267b7c36533d02ad6e2d
notte779 <mlp15amity@gmail.com>
2021-12-29 00:54:23 +0000

    Update public/words

--- app/controllers/public/words_controller.rb
+++ app/controllers/public/words_controller.rb
@@ -1,13 +1,27 @@
 class Public::WordsController < ApplicationController
   def index
+    @words = Word.all
   end
 
   def show
+    @word = Word.find(params[:id])
   end
 
   def new
+    @word = Word.new
   end
 
   def create
+    @word = Word.new(word_params)
+    if @word.save
+      redirect_to root_path, notice: '新規作成しました'
+    else
+      render :new
+    end
   end
+
+  private
+    def word_params
+      params.require(:word).permit(:body)
+    end
 end
